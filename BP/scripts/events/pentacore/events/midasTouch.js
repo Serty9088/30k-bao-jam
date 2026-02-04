ed3b50e0451d4a79e83dcfeeb922db20f8b05db7
VoidCell <145791590+VoidCelll@users.noreply.github.com>
2026-02-04 17:24:33 -0500

    Create midasTouch.js
    first version of Midas Touch.

---
+++ BP/scripts/events/pentacore/events/midasTouch.js
@@ -0,0 +1,61 @@
+import * as f from "../functions.js";
+import { world, system } from "@minecraft/server";
+
+LuckyEventType.register({
+    id: 'MidasTouch'
+    callback: (event => { // callback is the name of the property that describes the function
+        const player = event.player;
+
+        const dim = player.dimension
+        player.midasTouch = system.currentTick; world.sendMessage(`${player.name} has the §gMidas Touch!`)
+
+        function particleAndSound(loc) {
+            dim.spawnParticle('minecraft:ice_evaporation_emitter', loc); dim.playSound('dig.stone', loc, { pitch: 1.20 })
+        }
+
+        const midasTick = system.runInterval(() => {
+            for (const player of world.getAllPlayers()) {
+
+                if (player.midasTouch !== undefined) {
+                    const timeLeft = system.currentTick - player.midasTouch
+                    const block = player.getBlockStandingOn()
+                    if (block) {
+                        if (block.typeId !== 'minecraft:gold_block') {
+                            block.setType('minecraft:gold_block')
+                            particleAndSound(block?.center())
+                        }
+                    }
+                    if (timeLeft >= 20 * 20) {
+                        player.midasTouch = undefined
+                        system.clearRun(midasTick);
+                        world.afterEvents.entityHitEntity.unsubscribe(hitEntity)
+                        world.afterEvents.entityHitBlock.unsubscribe(hitBlock)
+                        world.sendMessage(`${player.name} no longer has the §gMidas Touch!`)
+                    }
+
+                }
+            }
+        }, 5)
+
+        const hitEntity = world.afterEvents.entityHitEntity.subscribe(({ hitEntity: target, damagingEntity: attacker }) => {
+            if (attacker.midasTouch !== undefined && target.typeId !== 'minecraft:player') {
+                particleAndSound(target.location)
+                attacker.dimension.getBlock(target.location).setType('minecraft:gold_block')
+                target.remove();
+            }
+
+        }, { entityTypes: ['minecraft:player'] })
+
+        const hitBlock = world.afterEvents.entityHitBlock.subscribe(({ hitBlock: block, damagingEntity: entity }) => {
+
+            if (entity.midasTouch !== undefined && block.typeId !== 'minecraft:player') {
+                if (block.typeId !== 'minecraft:gold_block') {
+                    particleAndSound(block.center())
+                    block.setType('minecraft:gold_block')
+                }
+            }
+
+        }, { entityTypes: ['minecraft:player'] })
+
+    })
+});
